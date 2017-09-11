c308cd2575a0ab4f1019ef06d9c1b78d4b0306f8
Maxime Morisset <maxime.morisset@augmentedev.com>
2017-09-12 01:03:48 +0200

    Add more templating for projects

--- app/javascripts/custom/carousel-init.js
+++ app/javascripts/custom/carousel-init.js
@@ -1,78 +1,57 @@
-
-/*global $:false */
-/*global window: false */
-
 (function(){
   "use strict";
-
-
-$(function ($) {
-
-   
-
-
-
-
+  $(function ($) {
     $("#testimonial-carousel").owlCarousel({
-        navigation : false,
-        pagination: true,
-        responsive: true,
-        items: 1,
-        touchDrag: true,
-        navigationText: false,
-        mouseDrag: true,
-        itemsDesktop: [3000,1],
-        itemsDesktopSmall: [1440,1],
-        itemsTablet:[1024,1],
-        itemsTabletSmall: [600,1],
-        itemsMobile: [360,1],
-        autoPlay: false,
-        autoHeight: true,
-      });
-
-
-    
+      navigation : false,
+      pagination: true,
+      responsive: true,
+      items: 1,
+      touchDrag: true,
+      navigationText: false,
+      mouseDrag: true,
+      itemsDesktop: [3000,1],
+      itemsDesktopSmall: [1440,1],
+      itemsTablet:[1024,1],
+      itemsTabletSmall: [600,1],
+      itemsMobile: [360,1],
+      autoPlay: false,
+      autoHeight: true,
+    });
 
     $("#team-carousel").owlCarousel({
-        navigation : true,
-        pagination: false,
-        responsive: true,
-        items: 1,
-        touchDrag: true,
-        navigationText: false,
-        mouseDrag: true,
-        itemsDesktop: [3000,3],
-        itemsDesktopSmall: [1440,3],
-        itemsTablet:[1024,3],
-        itemsTabletSmall: [640,1],
-        itemsMobile: [360,1],
-        autoPlay: false,
-        autoHeight: false,
-      });
+      navigation : true,
+      pagination: false,
+      responsive: true,
+      items: 1,
+      touchDrag: true,
+      navigationText: false,
+      mouseDrag: true,
+      itemsDesktop: [3000,3],
+      itemsDesktopSmall: [1440,3],
+      itemsTablet:[1024,3],
+      itemsTabletSmall: [640,1],
+      itemsMobile: [360,1],
+      autoPlay: false,
+      autoHeight: false,
+    });
 
-    
     $("#thumb-carousel").owlCarousel({
-        navigation : true,
-        pagination: false,
-        responsive: true,
-        items: 2,
-        touchDrag: true,
-        mouseDrag: true,
-        itemsDesktop: [3000,8],
-        itemsDesktopSmall: [1440,2],
-        itemsTablet:[800,2],
-        itemsTabletSmall: [600,2],
-        itemsMobile: [360,1],
-        navigationText: false,
-        autoPlay: true
-      });
-
-
-});
-// $(function ($)  : ends
-
+      navigation : true,
+      pagination: false,
+      responsive: true,
+      items: 2,
+      touchDrag: true,
+      mouseDrag: true,
+      itemsDesktop: [3000,8],
+      itemsDesktopSmall: [1440,2],
+      itemsTablet:[800,2],
+      itemsTabletSmall: [600,2],
+      itemsMobile: [360,1],
+      navigationText: false,
+      autoPlay: true
+    });
+  });
 })();
-//  JSHint wrapper $(function ($)  : ends
 
 
 
@@ -80,5 +59,5 @@ $(function ($) {
 
 
 
-  
+
 
